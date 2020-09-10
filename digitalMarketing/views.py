206507d2732fa3220e209d740dab3e6e687cf3e2
darylkangjh <daryl.kangjh@gmail.com>
2020-09-10 15:33:09 +0000

    Test written in wrong app. Changed test to Home app and doing testing

--- digitalMarketing/views.py
+++ digitalMarketing/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 
-
-
 # 02. E-Commerce show all products!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 def dmAll(request):
     return HttpResponse("Hello")
