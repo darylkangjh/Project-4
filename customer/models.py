35e5fa8b55f98988c089ced9ee11d16275a21221
darylkangjh <daryl.kangjh@gmail.com>
2020-09-10 15:58:47 +0000

    editcustomer field to be stored together with User (provided by Django)

--- customer/models.py
+++ customer/models.py
@@ -5,10 +5,9 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     
     name = models.CharField(blank=False, max_length=255)
-    email = models.CharField(blank=False, max_length=255)
     company = models.CharField(blank=True, null=True, max_length=255)
     created_on = models.DateTimeField(auto_now_add=True, blank=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return customer.name
