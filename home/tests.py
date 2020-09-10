f353b6ad1962adff2c80765d131d6738a394943c
darylkangjh <daryl.kangjh@gmail.com>
2020-09-10 15:41:00 +0000

    test failed as home-template not created

--- home/tests.py
+++ home/tests.py
@@ -6,3 +6,4 @@ class TestViews(TestCase):
     def test_get_home_page(self):
         response=self.client.get('/')
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.template.html')
