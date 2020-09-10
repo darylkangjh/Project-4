2d2adec9b6813060f65752a23538018d80958307
darylkangjh <daryl.kangjh@gmail.com>
2020-09-10 15:43:14 +0000

    Test failed as home.template.html and template folder not created

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home page here")
+    return HttpResponse(request, 'home.template.html')
