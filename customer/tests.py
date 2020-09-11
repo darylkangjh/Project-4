f0baa86e5e0d30dd204bba12923e0fe7bd3059fb
darylkangjh <daryl.kangjh@gmail.com>
2020-09-11 05:49:56 +0000

    Tested for all-customer url! All ok!

--- customer/tests.py
+++ customer/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestViews(TestCase):
+
+    def test_get_all_customer_page(self):
+        response = self.client.get('/all-customer/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, 'customer/all-customer.template.html')
--- home/tests.py
+++ home/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
 class TestViews(TestCase):
-    
+
     def test_get_home_page(self):
-        response=self.client.get('/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.template.html')
