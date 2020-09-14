4ef019380f723daa2c2b5f3f17f23968f45b10a8
darylkangjh <daryl.kangjh@gmail.com>
2020-09-14 05:35:58 +0000

    test views created for DA Services (failed as expected)

--- digitalMarketing/tests.py
+++ digitalMarketing/tests.py
@@ -10,3 +10,9 @@ class DMServicesTestCase(TestCase):
     def test_can_get_DMService_form(self):
         response = self.client.get('/dmservice/create')
         self.assertEqual(response.status_code, 200)
+
+class DAServicesTestCase(TestCase):
+
+    def test_can_get_DAService_form(self):
+        response = self.client.get('/daservice/create')
+        self.assertEqual(response.status_code, 200)
--- digitalMarketing/views.py
+++ digitalMarketing/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render, HttpResponse
 from .forms import DMServiceForm
 
 # Create your views here.
+
+
+# DmServices Views here 
+# !!! ... Create route for DMServices
 def create_DMService(request):
     create_form = DMServiceForm()
     return render(request, 'digitalMarketing/create_dmservice.template.html', {
         'form':create_form
     })
+
+
+# DAServices Views here
+#!!!... Create route for DaServices
 
 # 02. E-Commerce show all products!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
