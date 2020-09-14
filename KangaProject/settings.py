ac6fca8e1ac239d86670ef718ce5ce7f93cf73e7
darylkangjh <daryl.kangjh@gmail.com>
2020-09-14 06:02:37 +0000

    installed Django Taggit

--- KangaProject/settings.py
+++ KangaProject/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'crispy_forms',
+    'taggit',
     # Add my apps below
     'digitalMarketing',
     'customer',
--- digitalMarketing/models.py
+++ digitalMarketing/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from taggit.managers import TaggableManager
 # Create your models here.
 
 
