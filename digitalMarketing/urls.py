d432a9b95769b558185617f128c2a4faadf7fc65
darylkangjh <daryl.kangjh@gmail.com>
2020-10-01 15:47:39 +0000

    url error, amended duplicate and url name

--- digitalMarketing/urls.py
+++ digitalMarketing/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 import digitalMarketing.views
 
 urlpatterns = [
-    path('services/all', digitalMarketing.views.all_service,
+    path('/all', digitalMarketing.views.all_service,
          name='all_services'),
-    path('dmservice/create', digitalMarketing.views.create_DMService, name = 'create_dmservice'),
-    path('dmservice/create', digitalMarketing.views.create_DMService, name = 'create_dmservice'),
-    path('dmservice/update/<DMService_id>', digitalMarketing.views.update_DMService, name = 'update_dmservice'),
-    path('dmservice/delete/<DMService_id>', digitalMarketing.views.delete_DMService, name = 'delete_dmservice'),   
+    path('/create', digitalMarketing.views.create_DMService, name = 'create_dmservice'),
+    path('/update/<DMService_id>', digitalMarketing.views.update_DMService, name = 'update_dmservice'),
+    path('/delete/<DMService_id>', digitalMarketing.views.delete_DMService, name = 'delete_dmservice'),   
 ]
