b866123609564efe496ef97024d4c090281e9cad
darylkangjh <daryl.kangjh@gmail.com>
2020-10-01 15:50:51 +0000

    leading slash removed!

--- digitalMarketing/urls.py
+++ digitalMarketing/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 import digitalMarketing.views
 
+
 urlpatterns = [
-    path('/all', digitalMarketing.views.all_service,
+    path('all', digitalMarketing.views.all_service,
          name='all_services'),
-    path('/create', digitalMarketing.views.create_DMService, name = 'create_dmservice'),
-    path('/update/<DMService_id>', digitalMarketing.views.update_DMService, name = 'update_dmservice'),
-    path('/delete/<DMService_id>', digitalMarketing.views.delete_DMService, name = 'delete_dmservice'),   
+    path('create', digitalMarketing.views.create_DMService, name = 'create_dmservice'),
+    path('update/<DMService_id>', digitalMarketing.views.update_DMService, name = 'update_dmservice'),
+    path('delete/<DMService_id>', digitalMarketing.views.delete_DMService, name = 'delete_dmservice'),   
 ]
