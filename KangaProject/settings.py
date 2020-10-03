08faa1202e751bb268582272dcf8c748ffa30cae
darylkangjh <daryl.kangjh@gmail.com>
2020-10-03 10:21:14 +0000

    updated email

--- KangaProject/settings.py
+++ KangaProject/settings.py
@@ -111,6 +111,19 @@ LOGIN_REDIRECT_URL = '/success'
 WSGI_APPLICATION = 'KangaProject.wsgi.application'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# simulate Django to send out emails
+TEST_EMAIL = os.environ.get("TEST_EMAIL")
+if TEST_EMAIL == "1":
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+    
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
